fa18c6dca700797646c3d8d846fa21243d1afc54
Joris Calabrese <joris.calabrese@gmail.com>
2013-09-20 00:14:29 +0200

    Update some route

--- src/Lks/UserManagementBundle/Controller/DefaultController.php
+++ src/Lks/UserManagementBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction(Request $request)
+    public function createAction(Request $request)
     {
     	$member = new Member();
 
@@ -27,6 +27,7 @@ class DefaultController extends Controller
     		$em = $this->getDoctrine()->getManager();
 		    $em->persist($member);
 		    $em->flush();
+
 		    //TODO : Define a route
 		    return $this->render('LksUserManagementBundle:Default:yata.html.twig');
     	}
@@ -36,12 +37,36 @@ class DefaultController extends Controller
         ));
     }
 
-    /**
-     * @Route("/task_success", name="_security_check")
-     */
-    public function securityCheckAction()
+    public function listAndCreateAction(Request $request)
     {
+        //get all user items
+        $repository = $this->getDoctrine()
+            ->getRepository('LksUserManagementBundle:Member');
+
+        $members = $repository->findAll();
+
+        //begin of the form building
+        $member = new Member();
+
+        $form = $this->createFormBuilder($member)
+            ->add('firstname', 'text')
+            ->add('lastname', 'text')
+            ->add('save', 'submit')
+            ->getForm();
+
+        //mamage the response of the form
+        $form->handleRequest($request);
+
+        if($form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($member);
+            $em->flush();
+
+            //TODO : Define a route
+            return $this->render('LksUserManagementBundle:Default:listUser.html.twig', array('members' => $members));
+        }
         // The security layer will intercept this request
-        return $this->render('LksUserManagementBundle:Default:yata.html.twig');
+        return $this->render('LksUserManagementBundle:Default:listUser.html.twig', array('members' => $members, 'form' => $form));
     }
 }
